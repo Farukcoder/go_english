660d00da4a6727d3d0e7ec7ade802b75319bc77a
smartThinkFaruk <67540985+Farukcoder@users.noreply.github.com>
2025-07-22 11:57:16 +0600

    investment calculator done

--- invastment_calculator.go
+++ invastment_calculator.go
@@ -6,12 +6,23 @@ import (
 )
 
 func main() {
-	investmentAmount, years := 1000.0, 10.0
-	expectedReturnRate := 5.5 // in percentage
+	const inflationRate = 6.5 // in percentage
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Println("Investment Amount:")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Println("Expected Return Rate (in %): ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Println("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 
-	futureRealValue := futureValue / math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("Future Value of Investment: $", futureValue)
 	fmt.Println("Future Real Value of Investment: $", futureRealValue)
